dc887b8ccf6a236edae73266c77198c080604573
Thalyta Diniz <thalytadiniz03@gmail.com>
2025-02-10 16:58:48 -0300

    feat: savedimage logic impelemented

--- shop-cart/shop-cart/src/main/java/com/ecommerce/shop_cart/service/image/ImageService.java
+++ shop-cart/shop-cart/src/main/java/com/ecommerce/shop_cart/service/image/ImageService.java
@@ -1,7 +1,9 @@
 package com.ecommerce.shop_cart.service.image;
 
+import com.ecommerce.shop_cart.dto.ImageDto;
 import com.ecommerce.shop_cart.exceptions.ResourceNotFoundException;
 import com.ecommerce.shop_cart.model.Image;
+import com.ecommerce.shop_cart.model.Product;
 import com.ecommerce.shop_cart.repository.ImageRepository;
 import com.ecommerce.shop_cart.service.product.InterfaceProductService;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +13,8 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.sql.rowset.serial.SerialBlob;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -34,8 +38,37 @@ public class ImageService implements InterfaceImageService{
     }
 
     @Override
-    public Image saveImage(MultipartFile file, Long productId) {
-        return null;
+    public List<ImageDto> saveImages( Long productId,   List<MultipartFile> files) {
+        Product product = productService.getProductById(productId);
+
+        List<ImageDto> savedImageDto = new ArrayList<>();
+        for (MultipartFile file : files) {
+            try {
+                Image image = new Image();
+                image.setFileName(file.getOriginalFilename());
+                image.setFileType(file.getContentType());
+                image.setImage(new SerialBlob(file.getBytes()));
+                image.setProduct(product);
+
+                String buildDownloadUrl = "/api/v1/images/image/download/";
+                String downloadUrl = buildDownloadUrl+image.getId();
+                image.setDownloadUrl(downloadUrl);
+                Image savedImage = imageRepository.save(image);
+
+                savedImage.setDownloadUrl(buildDownloadUrl+savedImage.getId());
+                imageRepository.save(savedImage);
+
+                ImageDto imageDto = new ImageDto();
+                imageDto.setId(savedImage.getId());
+                imageDto.setFileName(savedImage.getFileName());
+                imageDto.setDownloadUrl(savedImage.getDownloadUrl());
+                savedImageDto.add(imageDto);
+
+            }   catch(IOException | SQLException e){
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        return savedImageDto;
     }
 
     @Override
--- shop-cart/shop-cart/src/main/java/com/ecommerce/shop_cart/service/image/InterfaceImageService.java
+++ shop-cart/shop-cart/src/main/java/com/ecommerce/shop_cart/service/image/InterfaceImageService.java
@@ -1,12 +1,13 @@
 package com.ecommerce.shop_cart.service.image;
 
+import com.ecommerce.shop_cart.dto.ImageDto;
 import com.ecommerce.shop_cart.model.Image;
 import org.springframework.web.multipart.MultipartFile;
+import java.util.List;
 
 public interface InterfaceImageService {
     Image getImageById(Long id);
     void deleteImageById(Long id);
-    Image saveImage(MultipartFile file, Long productId);
-    void updateImage(MultipartFile file, Long productId);
-
+    List<ImageDto> saveImages(Long productId, List<MultipartFile> files);
+    void updateImage(MultipartFile file,  Long imageId);
 }
